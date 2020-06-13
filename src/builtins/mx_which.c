0061d70f8d6285811291c2c57e068e027b017f57
andrii.hrynko <andrii.hrynko@takeoff.com>
2020-06-13 13:07:52 +0300

    fix for which

--- src/builtins/mx_which.c
+++ src/builtins/mx_which.c
@@ -1,8 +1,7 @@
 #include "ush.h"
 
-static int to_access(char *args, int flag) {
+static void to_access(char *args) {
     char *tmp = NULL;
-    int f = 1;
     char *path = getenv("PATH");
     char **m = NULL;
 
@@ -10,63 +9,49 @@ static int to_access(char *args, int flag) {
     for (int i = 0; m[i]; i++) {
         tmp = mx_strjoin(m[i], "/");
         tmp = mx_delit_fre(tmp, args);
-        if (to_access(tmp, F_OK) == 0) {
-            flag != 3 ? printf("%s\n", tmp) : 0;
-            f = 0;
-            if (flag == 0)
-                break ;
-        }
+        if (i == 3)
+            printf("%s\n", tmp);
         mx_strdel(&tmp);
     }
     mx_strdel(&tmp);
     mx_del_strarr(&m);
-    return f;
 }
 
 static int builtin_check(char *args) {
     char *built[] = {"cd", "pwd", "exit", "which", "env", "fg", 
         "processes", "export", "unset", "false", "true", NULL};
-    
+
     for (int i = 0; built[i]; i++) {
         if (!mx_strcmp(built[i], args)) {
-            //printf("%s: shell built-in command\n", built[i]);
+            printf("%s: shell built-in command\n", built[i]);
             return 0;
         }
     }
-    return 2;
+    return 1;
 }
 
 int mx_which(char **args) {
-    int f = 1;
-    int flag = 0;
+    int error_code = 0;
 
     if (args[1] == NULL)
         return 1;
-    if (args[1][0] == '-' && args[1][1] == 'a') {
-        //args++;
-        flag = 1;
-       //for (args++; *args; args++) {
-           f = 1;
-            if (builtin_check(*args)) {
-                f = to_access(*args, flag);
-                if (f == 1) {
-                    mx_printstr(*args);
-                    mx_printstr(" not found\n");
-                }
-            }
-       // }
-    }
-    else if (args[1][0] == '-' && args[1][1] == 's') {
-        flag = 0;
-        f = 0;
-        if (builtin_check(*args)) {
-            f = to_access(*args, flag);
-            if (f == 1) {
-                mx_printstr(*args);
+    if (args[1][0] == '-' && args[1][1] == 'a' && args[2]) {
+            if (builtin_check(args[2])) {
+                mx_printstr(args[2]);
                 mx_printstr(" not found\n");
+                error_code = 1;
             }
+            if (error_code == 0)
+                to_access(args[2]);
+    }
+    else if (args[1][0] == '-' && args[1][1] == 's' && args[2]) {
+        if (builtin_check(args[2])) {
+            mx_printstr(args[2]);
+            mx_printstr(" not found\n");
+            error_code = 1;
         }
     }
-
-    return f;
+    else if (args[2] == NULL)
+        error_code = 1;
+    return error_code;
 }
