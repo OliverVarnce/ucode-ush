2765c9fb3a281f1368aa72510bebdd8f558c9775
vzhukovsky <jukovskiyv96@gmail.com>
2020-06-15 19:44:02 +0300

    env

--- src/builtins/mx_which.c
+++ src/builtins/mx_which.c
@@ -6,23 +6,28 @@ static void mx_print_path(char *args) {
     char **m = NULL;
     
     m = mx_strsplit(path, ':');
-    tmp = mx_strjoin(m[3], "/");
+    tmp = mx_strjoin(m[2], "/");
     tmp = mx_delit_fre(tmp, args);
     printf("%s\n", tmp);
     mx_strdel(&tmp);
     mx_del_strarr(&m);
 }
 
-static void print_builtin_msg(char *args, int *err_code, int flag_a,
+static void print_builtin_msg(char *args, int *err_code, int flag_a, int flag_s,
         int *f_pass) {
     char *built[] = {"cd", "pwd", "exit", "which", "env", "fg",
-        "processes", "export", "unset", "false", "true", NULL};
+        "jobs", "export", "unset", "false", "true", NULL};
     int built_in = 0;
 
     for (int i = 0; built[i]; i++) {
         if (!mx_strcmp(built[i], args)) {
-            printf("%s: shell built-in command\n", args);
-            if (flag_a)
+            if (flag_a && i == 7)
+                printf("%s: export: shell reserved word\n", args);
+            if (i != 4)
+                printf("%s: shell built-in command\n", args);
+            if (flag_a && i != 7)
+                mx_print_path(args);
+            if (flag_s))
                 mx_print_path(args);
             built_in = 1;
             if (*f_pass == 1)
@@ -39,6 +44,7 @@ static void print_builtin_msg(char *args, int *err_code, int flag_a,
 int mx_which(char **args) {
     int err_code = 1;
     int flag_a = 0;
+    int flag_s = 0;
     int f_pass = 1;
     int i = 1;
 
@@ -48,9 +54,11 @@ int mx_which(char **args) {
         flag_a = 1;
         i++;
     }
-    else if (!mx_strcmp(args[i], "-s"))
+    else if (!mx_strcmp(args[i], "-s")) {
+        flag_s = 1;
         i++;
+    }
     for (; args[i]; i++)
-        print_builtin_msg(args[i], &err_code, flag_a, &f_pass);
+        print_builtin_msg(args[i], &err_code, flag_a, flag_s, &f_pass);
     return err_code;
 }
